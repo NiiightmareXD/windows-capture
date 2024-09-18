f42feef3b18d183ef0e69b825ef95dcdc584551b
MilkClouds <milkclouds00@gmail.com>
2024-09-19 00:56:37 +0900

    feat: width, height, path configuration by cli

--- examples/cli.rs
+++ examples/cli.rs
@@ -23,6 +23,14 @@ use windows_capture::{
 
 use windows::Graphics::Capture::GraphicsCaptureItem;
 
+// Struct to hold capture settings
+struct CaptureSettings {
+    stop_flag: Arc<AtomicBool>,
+    width: u32,
+    height: u32,
+    path: String,
+}
+
 // This struct will be used to handle the capture events.
 struct Capture {
     // The video encoder that will be used to encode the frames.
@@ -31,33 +39,33 @@ struct Capture {
     start: Instant,
     // To count the number of frames captured
     frame_count: u64,
-    // Flag to check if recording should stop
-    stop_flag: Arc<AtomicBool>,
+    // Capture settings including stop flag and video settings
+    settings: CaptureSettings,
 }
 
 impl GraphicsCaptureApiHandler for Capture {
     // The type of flags used to get the values from the settings.
-    type Flags = Arc<AtomicBool>;
+    type Flags = CaptureSettings;
 
     // The type of error that can occur during capture, the error will be returned from `CaptureControl` and `start` functions.
     type Error = Box<dyn std::error::Error + Send + Sync>;
 
     // Function that will be called to create the struct. The flags can be passed from settings.
-    fn new(stop_flag: Self::Flags) -> Result<Self, Self::Error> {
+    fn new(settings: Self::Flags) -> Result<Self, Self::Error> {
         println!("Capture started.");
 
         let encoder = VideoEncoder::new(
-            VideoSettingsBuilder::new(1920, 1080),
+            VideoSettingsBuilder::new(settings.width, settings.height),
             AudioSettingsBuilder::default().disabled(true),
             ContainerSettingsBuilder::default(),
-            "video.mp4",
+            &settings.path,
         )?;
 
         Ok(Self {
             encoder: Some(encoder),
             start: Instant::now(),
             frame_count: 0,
-            stop_flag,
+            settings,
         })
     }
 
@@ -82,7 +90,7 @@ impl GraphicsCaptureApiHandler for Capture {
         self.encoder.as_mut().unwrap().send_frame(frame)?;
 
         // Stop the capture if stop_flag is set
-        if self.stop_flag.load(Ordering::SeqCst) {
+        if self.settings.stop_flag.load(Ordering::SeqCst) {
             // Finish the encoder and save the video.
             self.encoder.take().unwrap().finish()?;
 
@@ -123,6 +131,18 @@ struct Cli {
     /// Draw border settings: always, never, default
     #[arg(long, default_value = "default")]
     draw_border: String,
+
+    /// Video width
+    #[arg(long, default_value_t = 1920)]
+    width: u32,
+
+    /// Video height
+    #[arg(long, default_value_t = 1080)]
+    height: u32,
+
+    /// Output file path
+    #[arg(long, default_value = "video.mp4")]
+    path: String,
 }
 
 fn parse_cursor_capture(s: &str) -> CursorCaptureSettings {
@@ -153,21 +173,21 @@ fn start_capture<T>(
     capture_item: T,
     cursor_capture: CursorCaptureSettings,
     draw_border: DrawBorderSettings,
-    stop_flag: Arc<AtomicBool>,
+    settings: CaptureSettings,
 ) where
     T: TryInto<GraphicsCaptureItem>,
 {
-    let settings = Settings::new(
+    let capture_settings = Settings::new(
         capture_item,
         cursor_capture,
         draw_border,
         ColorFormat::Rgba8,
-        stop_flag.clone(),
+        settings,
     );
 
     // Starts the capture and takes control of the current thread.
     // The errors from handler trait will end up here
-    Capture::start(settings).expect("Screen Capture Failed");
+    Capture::start(capture_settings).expect("Screen Capture Failed");
 }
 
 fn main() {
@@ -187,14 +207,31 @@ fn main() {
         .expect("Error setting Ctrl-C handler");
     }
 
+    let capture_settings = CaptureSettings {
+        stop_flag: stop_flag.clone(),
+        width: cli.width,
+        height: cli.height,
+        path: cli.path.clone(),
+    };
+
     if let Some(window_name) = cli.window_name {
         let capture_item =
             Window::from_contains_name(&window_name).expect("Window not found!");
-        start_capture(capture_item, cursor_capture, draw_border, stop_flag);
+        start_capture(
+            capture_item,
+            cursor_capture,
+            draw_border,
+            capture_settings,
+        );
     } else if let Some(index) = cli.monitor_index {
         let capture_item =
             Monitor::from_index(usize::try_from(index).unwrap()).expect("Monitor not found!");
-        start_capture(capture_item, cursor_capture, draw_border, stop_flag);
+        start_capture(
+            capture_item,
+            cursor_capture,
+            draw_border,
+            capture_settings,
+        );
     } else {
         eprintln!("Either --window-name or --monitor-index must be provided");
         std::process::exit(1);
